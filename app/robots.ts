e2d731679a26ed1181761a42c33fb9e8fe828f11
0xhardman <1142783160@qq.com>
2025-07-16 11:28:16 +0800

    fix: update sitemap URL to include www subdomain in robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/api/', '/admin/'],
     },
-    sitemap: 'https://lxscale.xyz/sitemap.xml',
+    sitemap: 'https://www.lxscale.xyz/sitemap.xml',
   }
 }
